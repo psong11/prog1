18083d466d27fea980cbe54bba5d371e815f81db
Paul Song <paulsong24@gmail.com>
2022-02-22 17:57:37 -0500

    added some more structure

--- main.cpp
+++ main.cpp
@@ -3,17 +3,98 @@ using namespace std;
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <vector>
 
-typedef struct node node;
+//typedef struct node node;
 struct node
 {
-    int data;
-    node *next;
+    float coord[4];
 };
 
-int main()
+node create_Node()
 {
+    return
+}
+
+//typedef struct Graph Graph;
+struct Graph
+{
+    node nodes[];
+};
+
+Graph generate_graph(int n, int dim)
+{
+
     srand(time(NULL));
+
+    Graph graph;
+
+    if (dim == 1)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            node newnode = [ float(rand() / RAND_MAX), 0, 0, 0 ] graph.nodes[i] = node.coord[];
+        }
+    }
+    if (dim == 2)
+    {
+    }
+    if (dim == 3)
+    {
+    }
+    if (dim == 4)
+    {
+    }
+    else
+    {
+    }
+
+    return graph;
+}
+
+int main(int argc, char **argv)
+{
+    int n = int(argv[1]);
+    int trials = int(argv[2]);
+    int dim = int(argv[3]);
+
+    if (dim < 1 || dim > 4)
+    {
+        invalid_argument("Please enter a valid dimension.")
+    }
+
+    float average;
+    Graph mst;
+
+    // For 'trial' number of times...
+    for (int i = 0; i < trials; i++)
+    {
+        // Generate a random graph
+        Graph graph = generate_graph(n, dim);
+
+        // Find the MST weight of this random graph
+
+        // Integrate that weight into the average weight
+    }
+
+    // return the output in the form: average(average weight of mst over the trials) numpoints numtrials dimension
+
+    /* Let us create the following graph
+        2 3
+    (0)--(1)--(2)
+    | / \ |
+    6| 8/ \5 |7
+    | / \ |
+    (3)-------(4)
+            9     */
+    int graph[V][V] = {{0, 2, 0, 6, 0},
+                       {2, 0, 3, 8, 5},
+                       {0, 3, 0, 0, 7},
+                       {6, 8, 0, 0, 9},
+                       {0, 5, 7, 9, 0}};
+
+    // Print the solution
+    primMST(graph);
 
     return 0;
 }
